d21187afada5bb6ef961c1a3922d457e281b6d12
KasthuriKrishna <22csy018@skcet.ac.in>
2024-06-15 12:13:44 +0530

    Initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me sitting with a laptop"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and APIs
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Cloud Computing Enthusiast</h3>
+              <p>
+                I have hands on experience in managing cloud resources.
+                Deployed and managed applications in AWS and Google Cloud.
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Software Tester</h3>
+              <p>
+                I have tested the UI and backend functionalities of various web applications using Selenium.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
